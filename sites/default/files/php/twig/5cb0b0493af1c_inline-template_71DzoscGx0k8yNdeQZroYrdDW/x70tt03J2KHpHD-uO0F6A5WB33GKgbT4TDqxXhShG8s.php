2cae4f88db5f78d5f93f92032660e6a0bf88f919
M13K <ag.koffi@hotmail.fr>
2019-04-19 14:47:03 +0200

    ajout de user6.sql

---
+++ sites/default/files/php/twig/5cb0b0493af1c_inline-template_71DzoscGx0k8yNdeQZroYrdDW/x70tt03J2KHpHD-uO0F6A5WB33GKgbT4TDqxXhShG8s.php
@@ -0,0 +1,87 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* {# inline_template_start #}<strong>{% trans 'Query' %}</strong> */
+class __TwigTemplate_4f187dfbf7462580cfd6bfd8affe0fbed50efb6778d629fec84d63d7eb8ddcf6 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["trans" => 1];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['trans'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<strong>";
+        echo t("Query", array());
+        echo "</strong>";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<strong>{% trans 'Query' %}</strong>";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{# inline_template_start #}<strong>{% trans 'Query' %}</strong>", "{# inline_template_start #}<strong>{% trans 'Query' %}</strong>", "");
+    }
+}
